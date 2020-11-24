72886268dca5eb81cecdeb47a3b00514f1b3d32f
João Henrique <joaoenrique.san@gmail.com>
2020-11-23 23:11:02 -0300

    Fixed Origin points/ Started working on animations

--- Entities/people/mik.gd
+++ Entities/people/mik.gd
@@ -4,7 +4,7 @@ func _ready():
 	$animator.play("idle")
 	
 func pick_umbrella():
-	$animator.play("pick_umbrella")
+	$animator.play("pick-umbrella")
 	
 func drop_umbrella():
 	$animator.play_backwards("drop_umbrella")
